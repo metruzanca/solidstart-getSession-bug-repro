ed2020900b44202ad3bbe8f6b310946c3e6cb52a
Sam Zanca <samuele@zanca.dev>
2023-12-29 10:50:07 -0500

    Error checking

--- src/api/session.ts
+++ src/api/session.ts
@@ -6,14 +6,25 @@ import { EventHandlerRequest, H3Event, getCookie, unsealSession } from "vinxi/se
 
 const SESSION_SECRET = process.env.SESSION_SECRET ?? "areallylongsecretthatyoushouldreplace"
 
+type SessionDataT = Record<string, any>;
 // This is a workaround, see https://github.com/solidjs/solid-start/issues/1192
-export async function getSessionData<SessionDataT = Record<string, any>>() {
-  const event = getRequestEvent()!
-  const cookie = getCookie(event, "h3")!
-  const { data } = await unsealSession(
-    event as H3Event<EventHandlerRequest>,
-    { password: SESSION_SECRET },
-    cookie
-  )
-  return data! as SessionDataT
+export async function getSessionData<Data extends SessionDataT = SessionDataT>(): Promise<Data> {
+  try {
+    const event = getRequestEvent()
+    if (!event) throw new Error("Shouldn't happen, missing request event")
+
+    const cookie = getCookie(event, "h3")
+    if (!cookie) throw new Error("Missing cookie")
+
+    const { data } = await unsealSession(
+      event as H3Event<EventHandlerRequest>,
+      { password: SESSION_SECRET },
+      cookie
+    )
+    if (data) 
+      return data as unknown as Data
+  } catch (error) {
+    console.error(error)
+  }
+  return {} as unknown as Data
 }
